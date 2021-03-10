e90077aa6d304a9aaff4157d94815c97b15fa70f
Tony Roy <tonyroy@wcksoft.com>
2021-03-10 17:10:00 -0400

    Create netbox.php

---
+++ config/netbox.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'default' => env('NETBOX_CONNECTION', 'default'),
+
+    'panels' => [
+        'default' => [
+            'url' => env('NETBOX_DEFAULT_URL'),
+            'key' => env('NETBOX_DEFAULT_KEY'),
+        ],
+    ],
+
+    'client_options' => [
+        \GuzzleHttp\RequestOptions::COOKIES => true,
+        \GuzzleHttp\RequestOptions::CONNECT_TIMEOUT => 10,
+        \GuzzleHttp\RequestOptions::TIMEOUT => 10,
+        \GuzzleHttp\RequestOptions::ALLOW_REDIRECTS => false,
+    ],
+
+];
