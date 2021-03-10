a4f1ff7346a49c96e0dad153b3f20be64c7d9815
Tony Roy <tonyroy@wcksoft.com>
2021-03-10 18:40:36 -0400

    Create AbstractApi.php

---
+++ src/Api/AbstractApi.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace wickedsoft\NetBox\Api;
+
+use wickedsoft\NetBox\Client;
+
+abstract class AbstractApi implements ApiInterface
+{
+    /** @var \wickedsoft\NetBox\Client */
+    public $client;
+
+    /**
+     * AbstractApi constructor.
+     * @param \wickedsoft\NetBox\Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function post($parameters)
+    {
+        return $this->client->getHttpClient()->post(array_merge([
+            'api_key' => $this->client->getHttpClient()->getOptions()['key']
+        ], $parameters));
+    }
+}
